8894d3376944a92227244dd41e691b57ac228c9d
Dylan Mace <dtmace2@gmail.com>
2021-07-23 11:41:20 -0400

    add conda env

--- tmp.py
+++
@@ -1,6 +0,0 @@
-from tkinter import ttk  # Normal Tkinter.* widgets are not themed!
-from ttkthemes import ThemedTk
-
-window = ThemedTk(theme="equilux")
-ttk.Button(window, text="Quit", command=window.destroy).pack()
-window.mainloop()
